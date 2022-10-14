1e72506ccc59545d341fd5c34e867ac4de3ffe73
Brendon Allen Curry-Hobbs <46946074+brendonc-h@users.noreply.github.com>
2022-10-13 18:30:34 -0600

    db commit

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-const PORT = process.allowedNodeEnvironmentFlags.PORT || 3001;
+const uuid = require('./helpers/uuid.js')
+
+const PORT = process.env.PORT || 3001;
 
 //get express middleware 
 const app = express();
@@ -11,3 +15,30 @@ app.use(require(".routes"));
 app.listen(PORT, () =>
 console.log('App listening at local host')
 );
+
+// get routes for the homepage
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// route for api notes for notes.html
+app.get('/api/notes', (req,res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+// get route for db.json file
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/db/db.json'))
+});
+// searched route
+
+app.get('/api/notes/:id', (req, res) => {
+    const choiceNote = req.params.id;
+    res.json(choiceNote)
+});
+
+// post creating notes
+app.post('/api/notes', (req,res) => {
+    const makeNote = req.body;
+    makeNote.id = uuid();
+
+    let 
+})
